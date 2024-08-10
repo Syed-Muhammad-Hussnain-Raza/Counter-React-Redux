465520c3bfb3c7cfa9dc43cdd7f5b54493508a81
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-10 23:09:34 +0500

    All the components are imported here

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Header from "./components/Header";
+import DisplayCounter from "./components/DisplayCounter";
+import Container from "./components/Container";
+import Controls from "./components/Controls";
 
 function App() {
-  return <></>;
+  return (
+    <center className="px-4 py-5 my-5 text-center">
+      <Container>
+        <Header></Header>
+        <div className="col-lg-6 mx-auto">
+          <DisplayCounter></DisplayCounter>
+          <Controls></Controls>
+        </div>
+      </Container>
+    </center>
+  );
 }
 
 export default App;
