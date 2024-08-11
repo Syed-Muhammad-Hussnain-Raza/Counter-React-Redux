93b3e547d715f25eec682d9b73a47dca38db1212
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-11 11:56:17 +0500

    Update - Added functionality for add, substract and privacyToggle buttons

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,8 +1,9 @@
-import { WiDayThunderstorm } from "react-icons/wi";
+import { useRef } from "react";
 import { useDispatch } from "react-redux";
 
 const Controls = () => {
   const dispatch = useDispatch();
+  const inputElement = useRef();
 
   const handleIncrement = () => {
     dispatch({ type: "INCREMENT" });
@@ -10,6 +11,32 @@ const Controls = () => {
 
   const handleDecrement = () => {
     dispatch({ type: "DECREMENT" });
+  };
+
+  const handleAdd = () => {
+    dispatch({
+      type: "ADD",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
+
+  const handleSubstract = () => {
+    dispatch({
+      type: "SUBSTRACT",
+      payload: {
+        num: inputElement.current.value,
+      },
+    });
+    inputElement.current.value = "";
+  };
+
+  const handlePrivacyToggle = () => {
+    dispatch({
+      type: "PRIVACY_TOGGLE",
+    });
   };
 
   return (
@@ -29,14 +56,33 @@ const Controls = () => {
         >
           -1
         </button>
+        <button
+          type="button"
+          className="btn btn-warning"
+          onClick={handlePrivacyToggle}
+        >
+          Privacy Toggle
+        </button>
       </div>
       <div
         className="d-grid gap-3 d-sm-flex justify-content-sm-center"
         style={{ margin: "10px" }}
       >
-        <input type="text" placeholder="Enter number" className="numberInput"/>
-        <button type="button" className="btn btn-info" >
+        <input
+          type="text"
+          placeholder="Enter number"
+          className="numberInput"
+          ref={inputElement}
+        />
+        <button type="button" className="btn btn-info" onClick={handleAdd}>
           Add
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger"
+          onClick={handleSubstract}
+        >
+          Substract
         </button>
       </div>
     </>
