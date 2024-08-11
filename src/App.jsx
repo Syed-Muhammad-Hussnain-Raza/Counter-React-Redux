0206b02e46d530f5a3ed481c9f1c31b0fc7e1f44
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-11 11:59:45 +0500

    Updated - Logic for Privacy Toggle defined here

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Header from "./components/Header";
 import DisplayCounter from "./components/DisplayCounter";
 import Container from "./components/Container";
 import Controls from "./components/Controls";
+import { useSelector } from "react-redux";
+import PrivacyMessage from "./components/PrivacyMessage";
 
 function App() {
+  const privacy = useSelector((store) => store.privacy);
+
   return (
     <center className="px-4 py-5 my-5 text-center">
       <Container>
-        <Header></Header>
+        <Header />
         <div className="col-lg-6 mx-auto">
-          <DisplayCounter></DisplayCounter>
-          <Controls></Controls>
+          {privacy ? <PrivacyMessage /> : <DisplayCounter />}
+          <Controls />
         </div>
       </Container>
     </center>
