b322a11bed12e4d646de72b238ed604ba0f9a424
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-11 12:01:17 +0500

    Updated - Handle reducer for dispatch of ADD, SUBSTRACT and PRIVACY_TOGGLE

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,38 @@
 import { createStore } from "redux";
 
-// Reducer
+// Initial value for store
 const INITIAL_VALUE = {
   counter: 0,
+  privacy: false,
+  author: "Syed Muhammad Hussnain Raza",
 };
 
+// Reducer for store
 const counterReducer = (store = INITIAL_VALUE, action) => {
   const newStore = store;
 
   if (action.type === "INCREMENT") {
-    return { counter: store.counter + 1 };
+    return { ...store, counter: store.counter + 1 };
   } else if (action.type === "DECREMENT") {
-    return { counter: store.counter - 1 };
+    return { ...store, counter: store.counter - 1 };
+  } else if (action.type === "ADD") {
+    return {
+      ...store,
+      counter: store.counter + Number(action.payload.num),
+    };
+  } else if (action.type === "SUBSTRACT") {
+    return {
+      ...store,
+      counter: store.counter - Number(action.payload.num),
+    };
+  } else if (action.type === "PRIVACY_TOGGLE") {
+    return { ...store, privacy: !store.privacy };
   }
 
   return newStore;
 };
 
-// Store
+// Store definition
 const counterStore = createStore(counterReducer);
 
 export default counterStore;
-
-// Actions are defined in Controls.jsx
