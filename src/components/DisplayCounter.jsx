9fa0bb6d259e33aa932658ed83376e1464dcfe25
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-11 20:46:33 +0500

    Code updated for new Store | Redux-Toolkit

--- src/components/DisplayCounter.jsx
+++ src/components/DisplayCounter.jsx
@@ -1,9 +1,9 @@
 import { useSelector } from "react-redux";
 
 const DisplayCounter = () => {
-  const counter = useSelector((store) => store.counter);
+  const { counterVal } = useSelector((store) => store.counter);
 
-  return <p className="lead mb-4">Counter Current Value: {counter}</p>;
+  return <p className="lead mb-4">Counter Current Value: {counterVal}</p>;
 };
 
 export default DisplayCounter;
