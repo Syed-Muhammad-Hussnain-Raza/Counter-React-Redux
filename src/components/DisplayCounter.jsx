c2f2944b0817a55a5e1f5de12460c069bbf8cc23
Syed-Muhammad-Hussnain-Raza <syedhussnainraza189@gmail.com>
2024-08-11 10:13:08 +0500

    useSelector hook used to connect this component with store to observe changes in value of counter on actions

--- src/components/DisplayCounter.jsx
+++ src/components/DisplayCounter.jsx
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux";
+
 const DisplayCounter = () => {
-  return <p className="lead mb-4">Counter Current Value</p>;
+  const counter = useSelector((store) => store.counter);
+
+  return <p className="lead mb-4">Counter Current Value: {counter}</p>;
 };
 
 export default DisplayCounter;
